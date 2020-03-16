96659d649914cadd75cd554889f5eaf0f0b84eab
senthil-nathan26 <snbca12@gmail.com>
2020-03-16 09:35:44 +0530

    updated

---
+++ src/test/java/logintest/senthilUpdated.java
@@ -0,0 +1,37 @@
+package logintest;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import loginpage.Basepage;
+import loginpage.Loginpage;
+
+public class Logintest {
+
+	Loginpage lgp1;
+	Basepage  bp;
+	
+	public Logintest()
+	{
+		lgp= new Loginpage();
+		bp = new Basepage();	
+	}
+	@Test
+	public void user()
+	{
+		lgp.user().sendKeys("Senthilks");
+		Assert.assertEquals(lgp.getTitlte().contains("username"),"Failed");
+	}
+	@Test
+	public void pass()
+	{
+		lgp.pass().sendKeys("743702");
+		Assert.assertEquals(lgp.getTitlte().contains("password"),"Failed");
+	}
+	@Test
+	public void btLogin()
+	{
+		lgp.clicks().click();
+	}
+	
+}
